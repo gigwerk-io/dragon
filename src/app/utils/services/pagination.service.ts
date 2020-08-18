8c999e43f4327a972044ffae977286572b63d3d0
moises <moimigueldev@gmail.com>
2020-08-17 19:40:06 -0500

    fix: 🐛 Implemented a pagination service to reduce code
    The pagination service succesfully removes around 60% of the repetitive
    pagination logic that is found in all of the pages that utilises tables
    or grids.
    
    BREAKING CHANGE: 🧨 none
    
    ✅ Closes: none

---
+++ src/app/utils/services/pagination.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaginationService {
+  activePage = 1;
+  maxPages: number;
+  maxPage: number;
+  pagination: number[];
+
+  constructor() { }
+
+  setupPagination(list: any[], windowSize: number) {
+    const maxPagesCalc = Math.round((list.length / windowSize) - ((list.length % windowSize) / windowSize));
+    this.maxPages = (maxPagesCalc * windowSize) >= list.length ? maxPagesCalc : (maxPagesCalc + 1);
+    if (this.maxPages < 19) {
+      this.pagination = Array(this.maxPages).fill(undefined).map((x, i) => i + 1);
+      this.maxPage = this.maxPages;
+    } else {
+      this.setActivePage(1, list);
+    }
+  }
+
+
+  setActivePage(page: number, list: any[]) {
+    this.activePage = page;
+    if (this.maxPages > 19) {
+      this.pagination = [1];
+      const pages = 17;
+      let maxPage = this.maxPages;
+      let step = 9;
+      if (list.length % 5 === 0) {
+        maxPage -= 1;
+      }
+      const centeredRange = Array(pages).fill(undefined)
+        .map(() => {
+          const i = page + step;
+          if (step > 0) {
+            step -= 1;
+          } else if (step === 0) {
+            step = -7;
+          } else if (i < maxPage) {
+            step += 1;
+          }
+
+          if (i > 1 && i < maxPage) {
+            return i;
+          }
+
+          return undefined;
+        });
+      this.pagination.push(...centeredRange.sort((a, b) => a - b).filter(i => i !== undefined));
+      this.pagination.push(maxPage);
+      this.maxPage = maxPage;
+    }
+  }
+}
--- src/app/utils/services/table.service.ts
+++ src/app/utils/services/table.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@angular/core';
-import { User } from '../interfaces/models/User';
-import { MarketplaceJob } from '../interfaces/models/MarketplaceJob';
-import { Application } from '../interfaces/models/Application';
-import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +8,6 @@ import { filter } from 'rxjs/operators';
 export class TableService {
 
   constructor() { }
-
 
   // THIS FUNCTION SHOULD BE USED FOR ALL ITEMS FILTERED;
   // FOR AN EXAMPLE VISIT JOBS-LIST COMPONENT
@@ -25,7 +20,7 @@ export class TableService {
 
     list = list.length ? list : fullList;
 
-    const needle = e.toLowerCase();
+    const needle = e.toLowerCase().replace(/ /g, '');
 
     // tslint:disable-next-line: no-shadowed-variable
     return filteredList = list.filter((filter) => {
@@ -40,81 +35,5 @@ export class TableService {
           .join('');
       return hayStack.includes(needle);
     });
-  }
-
-  filterJobsTable(e: string, jobs: MarketplaceJob[], allJobs: MarketplaceJob[]) {
-    let filteredJobs = jobs;
-
-    if (!e.length) {
-      return allJobs;
-    }
-
-    const needle = e.toLowerCase();
-
-    return filteredJobs = jobs.filter((filteredJob) => {
-      const hayStack =
-        (
-          filteredJob.customer.email +
-          filteredJob.price +
-          filteredJob.customer.first_name +
-          filteredJob.customer.last_name +
-          filteredJob.intensity +
-          filteredJob.views
-        )
-          .toLowerCase()
-          .split(' ')
-          .join('');
-      return hayStack.includes(needle);
-    });
-  }
-
-  filterPeopleGrid(e: string, people: User[], allPeople: User[]) {
-    let Freelancer = people;
-
-    if (!e.length) {
-      return allPeople;
-    }
-
-    people = !people.length ? allPeople : people;
-
-    const needle = e.toLowerCase();
-
-    return Freelancer = people.filter((filteredUsers) => {
-      const hayStack =
-        (
-          filteredUsers.first_name +
-          filteredUsers.last_name
-        )
-          .toLowerCase()
-          .split(' ')
-          .join('');
-      return hayStack.includes(needle);
-    });
-  }
-
-  filterApplicants(e: string, applicants: Application[], allApplicants: Application[]) {
-    let Freelancer = applicants;
-
-    if (!e.length) {
-      return allApplicants;
-    }
-
-    applicants = applicants.length ? applicants : allApplicants;
-
-    const needle = e.toLowerCase();
-
-    return Freelancer = applicants.filter((filteredUsers) => {
-      const hayStack =
-        (
-          filteredUsers.user.first_name +
-          filteredUsers.status.name +
-          filteredUsers.user.email
-        )
-          .toLowerCase()
-          .split(' ')
-          .join('');
-      return hayStack.includes(needle);
-    });
-
   }
 }
